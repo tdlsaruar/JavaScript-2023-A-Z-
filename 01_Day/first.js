e2280e3c8e981407662917ad557cb24fcb603306
tdlsaruar <saruarahmed321@gmail.com>
2023-03-25 02:33:34 -0600

    25/03/2023

---
+++ 01_Day/first.js
@@ -0,0 +1,3 @@
+console.log('hello javascript');
+// This code will output "hello javascript" to the console,
+// which you can view in your broser's developer tools .the console.log() function is used to log messages to the console, and the massage inside the parenheses is the messsage that will be logged.
---
+++ 02_Day/Variables and Data types.js
@@ -0,0 +1,39 @@
+// 4 Ways to Declare a JavaScript   Variables 
+// first using var 
+var first =' this is first use of variable var';
+// secondly using let 
+let second='this is use of let'
+
+// Aditionaly use of const 
+const lastone='here is const use ';
+
+// Cannot be reserved keyowrd
+// Should be meanigful
+// Cannot start with a number (1name)
+// Cannot contain a space or hypen  (-)
+// Are case 
+// var/let/const sing of variables 
+console.log(second);
+
+
+var a='3';
+var b='8';
+
+var c=a;
+// a=b;
+// b=c;
+
+console.log('a is '+a)
+// Always declare JavaScript variables with var,let, orconst.
+
+// The var keyword is used in all JavaScript code from 1995 to 2015.
+
+// The let and const keywords were added to JavaScript in 2015.
+
+// If you want your code to run in older browsers, you must use var.Always declare JavaScript variables with var,let, orconst.
+
+// The var keyword is used in all JavaScript code from 1995 to 2015.
+
+// The let and const keywords were added to JavaScript in 2015.
+
+// If you want your code to run in older browsers, you must use var.
